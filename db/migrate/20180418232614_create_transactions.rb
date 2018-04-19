068b025c3d8273e3f5075f81d3435151cabfea16
Nancy Chang <nancy.chang@berkeley.edu>
2018-04-18 17:43:06 -0700

    created transaction view and added routes

--- db/migrate/20180418232614_create_transactions.rb
+++ db/migrate/20180418232614_create_transactions.rb
@@ -1,6 +1,10 @@
 class CreateTransactions < ActiveRecord::Migration[5.1]
   def change
     create_table :transactions do |t|
+      t.string :description
+      t.decimal :amount
+      t.integer :user_id
+      t.integer :event_id
 
       t.timestamps
     end
