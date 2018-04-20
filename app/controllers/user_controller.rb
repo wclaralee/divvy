c920f57c268674aeb4c1816052d810b67a85bff4
Maggie Luo <maggieluo@Maggies-MacBook-Pro.local>
2018-04-19 20:58:42 -0700

    commit login to master

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,5 @@
 class UserController < ApplicationController
 	before_action :authenticate_user!
-<<<<<<< HEAD
 
 	def index
 		@users = User.all
@@ -9,10 +8,5 @@ class UserController < ApplicationController
 	def show
 		@user = User.find(params[:id])
 	end
-=======
-	def show
-		@user = User.find(params[:id])
-	end
 	
->>>>>>> 76bded3458be1f4fe05700407c7cfeec8e2f98a2
 end
